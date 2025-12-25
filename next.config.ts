f9a6658bbbb82c9fa28a3d37dc0eb11c09040b33
Dominik K. <dominik@rivo.gg>
2025-12-25 21:28:19 +0100

    fix: add / redirect

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,15 @@ const nextConfig: NextConfig = {
   experimental: {
     useCache: true,
   },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/login",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default withWorkflow(nextConfig);
